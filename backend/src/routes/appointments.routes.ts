5961a22cb18df68afc9ad2e8fff34e63c7c0afd6
Gabriel <perien.op@gmail.com>
2020-11-15 15:33:05 -0300

    Updates
    Criado repositorio e atualizado as rotas

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,34 +1,22 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import AppointmentsRepository from '../respositories/AppointmentsRepository';
+import { startOfHour, parseISO } from 'date-fns';
 
 const appointmentsRouter = Router();
-
-interface Appointment{
-  id: string;
-  provider: string;
-  date: Date;
-}
-
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parseDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointments.find(appointment => isEqual(parseDate, appointment.date));
 
-  if(findAppointmentInSameDate){
-    return response.status(400).json({message: 'This000'});
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parseDate);
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: 'This000' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parseDate,
-  };
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parseDate);
 
   return response.json(appointment);
 });
