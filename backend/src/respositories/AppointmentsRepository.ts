5063cef4f23e0fe92b7ecb6a7de03877397c2106
Gabriel <perien.op@gmail.com>
2020-11-21 16:26:32 -0300

    Updates

--- backend/src/respositories/AppointmentsRepository.ts
+++ backend/src/respositories/AppointmentsRepository.ts
@@ -1,35 +1,19 @@
-import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointment';
+import { EntityRepository, Repository } from 'typeorm';
 
-interface CreateAppointmentDTO{
-  provider: string;
-  date: Date;
-}
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
 
-class AppointmentsRepository {
+    const findAppointment = await this.findOne({
+      where: { date },
+    });
 
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
-  public all(): Appointment[] {
-    return this.appointments;
-  }
-
-  public findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(appointment => isEqual(date, appointment.date));
     return findAppointment || null;
+
   }
 
-  public create({provider, date}: CreateAppointmentDTO): Appointment {
-    const appointment = new Appointment({provider, date});
-
-    this.appointments.push(appointment);
-
-    return appointment;
-  }
 }
+
 
 export default AppointmentsRepository;
