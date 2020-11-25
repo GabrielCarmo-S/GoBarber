438aee410275c722820517253ba7122fd398d21d
Gabriel <perien.op@gmail.com>
2020-11-25 11:11:33 -0300

    Updates

--- backend/src/database/migrations/1606249953476-AlterProviderFieldToProviderId.ts
+++ backend/src/database/migrations/1606249953476-AlterProviderFieldToProviderId.ts
@@ -1,8 +1,22 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, TableColumn, TableForeignKey} from "typeorm";
 
 export class AlterProviderFieldToProviderId1606249953476 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropColumn('appointments', 'provider');
+      await queryRunner.addColumn('appointments', new TableColumn({
+        name: 'provider_id',
+        type: 'uuid',
+        isNullable: true,
+      }));
+
+      await queryRunner.createForeignKey('appoointments', new TableForeignKey({
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
