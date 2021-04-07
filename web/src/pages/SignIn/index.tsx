46f22dd2ae597e64a91526789318c1df04c81266
GabrielCarmo-S <perien.op@gmail.com>
2021-04-07 18:15:24 -0300

    Create SignIn Page and Updated style

---
+++ web/src/pages/SignIn/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="E-mail" type="text" />
+
+          <input placeholder="Senha" type="password" />
+
+          <button type="submit">Entrar</button>
+
+          <a href="forgot">Esqueci Minha Senha</a>
+        </form>
+
+        <a href="ssss">
+          <FiLogIn /> 
+          Criar Conta
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
+
+export default SignIn;
