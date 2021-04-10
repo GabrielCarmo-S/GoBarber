d382b4b29eb2bb2cc9ccd074c9b8d95681657db0
GabrielCarmo-S <perien.op@gmail.com>
2021-04-10 13:38:33 -0300

    Updated SignUp Page

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -7,7 +7,8 @@ import React, {
 } from 'react';
 import { useField } from '@unform/core';
 import { IconBaseProps } from 'react-icons';
-import { Container } from './styles';
+import { Container, Error } from './styles';
+import { FiAlertCircle } from 'react-icons/fi';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -39,7 +40,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField]);
 
   return (
-    <Container isFilled={isFilled} isFocused={isFocused}>
+    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocues}
@@ -49,7 +50,11 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         {...rest}
       />
 
-      {error}
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="#c53030" />
+        </Error>
+      )}
     </Container>
   );
 };
