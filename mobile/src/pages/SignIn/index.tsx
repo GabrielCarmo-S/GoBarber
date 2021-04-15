aae15ffded8dec26e717b18d8a27d105447d0d8c
GabrielCarmo-S <perien.op@gmail.com>
2021-04-15 14:48:01 -0300

    Updated Style Page SignIn

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,23 +1,52 @@
 import React from 'react';
-import {Container, Title} from './styles';
-
 import logoImg from '../../assets/logo.png';
-import {Image} from 'react-native';
+import {Image, KeyboardAvoidingView, Platform} from 'react-native';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
+import {ScrollView} from 'react-native-gesture-handler';
+
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled>
+        <ScrollView
+          contentContainerStyle={{flex: 1}}
+          keyboardShouldPersistTaps="handled">
+          <Container>
+            <Image source={logoImg} />
 
-      <Title>Faça seu logon</Title>
+            <Title>Faça seu logon</Title>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button onPress={() => {}}>Entrar</Button>
-    </Container>
+            <Button onPress={() => {}}>Entrar</Button>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci Minha Senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+          <CreateAccountButton onPress={() => {}}>
+            <Icon name="log-in" size={20} color="#ff9000" />
+            <CreateAccountButtonText>Criar uma Conta</CreateAccountButtonText>
+          </CreateAccountButton>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
