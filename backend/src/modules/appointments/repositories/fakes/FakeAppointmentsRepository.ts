1f7378bd384bb9ca67ef74e17592353021ec6257
GabrielCarmo-S <perien.op@gmail.com>
2021-04-26 13:55:13 -0300

    Updates

---
+++ backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,31 @@
+import ICreateAppointDTO from 'modules/appointments/dtos/ICreateAppointmentDTO';
+import Appointment from '../../infra/typeorm/entities/Appointment';
+import { v4 } from 'uuid';
+import IAppointmentsRepository from '../IAppointmentsRepository';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date
+    );
+
+    return findAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: v4(), date, provider_id });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
